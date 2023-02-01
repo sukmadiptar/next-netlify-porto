2557ffd1eac76771e5279c5f3ac7f0cefd652005
Sukma Pradipta R <sukmadiptar@gmail.com>
2023-02-01 17:11:28 +0700

    feat: add mobile menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,26 @@
-import { Box, Flex, Menu, Spacer, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import Image from 'next/image'
+import {
+  Flex,
+  Text,
+  useColorMode,
+  Switch,
+  IconButton,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure
+} from '@chakra-ui/react'
 import Link from 'next/link'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 export const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const isDark = colorMode === 'dark'
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <>
       <Flex
@@ -22,18 +40,73 @@ export const Navbar = () => {
             <Text color="white">LOGO</Text>
           </a>
         </Link>
-        <Flex as="nav">
-          <Text color="white" mx="4">
-            about
-          </Text>
-          <Text color="white" mx="4">
-            portfolio
-          </Text>
-          <Text color="white" mx="4">
-            contact
-          </Text>
-          <Image src="/netliheart.svg" width="30" height="30" alt="Instagram" />
-          <Image src="/netliheart.svg" width="30" height="30" alt="Github" />
+        <Flex gap={5} pos="revert">
+          <Flex as="nav" gap={8} display={['none', 'none', 'flex', 'flex']}>
+            <Text color="white">about</Text>
+            <Text color="white">portfolio</Text>
+            <Text color="white">contact</Text>
+            <Flex gap={5}>
+              <Link href="https://www.instagram.com/pradiptarr/" passHref>
+                <a target="_blank">
+                  <Image src="/icons/instagram.png" width="25" height="25" alt="Instagram" />
+                </a>
+              </Link>
+              <Link href="https://github.com/sukmadiptar" passHref>
+                <a target="_blank">
+                  <Image src="/icons/github.png" width="25" height="25" alt="Github" />
+                </a>
+              </Link>
+              <Link href="https://www.linkedin.com/in/sukma-pradipta-ramadhan/" passHref>
+                <a target="_blank">
+                  <Image src="/icons/linkedin.png" width="25" height="25" alt="Linkedin " />
+                </a>
+              </Link>
+            </Flex>
+          </Flex>
+          <IconButton
+            variant="ghost"
+            aria-label="Hamburger"
+            color="white"
+            icon={<HamburgerIcon />}
+            size="md"
+            pb={[0, 0, 3, 3]}
+            left="3"
+            top={['1.6', '1.6', '0', '0']}
+            display={['flex', 'flex', 'none', 'none']}
+            onClick={onOpen}
+          />
+          <Drawer isOpen={isOpen} placement="right" onClose={onClose} autoFocus size="xs">
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerBody bgColor="black">
+                <Flex gap={2} flexDir="column" py={6}>
+                  <Text color="white">about</Text>
+                  <Text color="white">portfolio</Text>
+                  <Text color="white">contact</Text>
+                  <Flex gap={5}>
+                    <Link href="https://www.instagram.com/pradiptarr/" passHref>
+                      <a target="_blank">
+                        <Image src="/icons/instagram.png" width="25" height="25" alt="Instagram" />
+                      </a>
+                    </Link>
+                    <Link href="https://github.com/sukmadiptar" passHref>
+                      <a target="_blank">
+                        <Image src="/icons/github.png" width="25" height="25" alt="Github" />
+                      </a>
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/sukma-pradipta-ramadhan/" passHref>
+                      <a target="_blank">
+                        <Image src="/icons/linkedin.png" width="25" height="25" alt="Linkedin " />
+                      </a>
+                    </Link>
+                  </Flex>
+                </Flex>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
+
+          <Switch m={[3, 3, 1, 1]} color="green" isChecked={isDark} onChange={toggleColorMode} />
         </Flex>
       </Flex>
     </>
